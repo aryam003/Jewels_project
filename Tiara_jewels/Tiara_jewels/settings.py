ca8ca84100a4e9c6f20312d7a8de25d80448fb5c
aryam003 <aryamarya537@gmail.com>
2025-01-07 13:40:09 +0530

    7/1/2025 updating

--- Tiara_jewels/Tiara_jewels/settings.py
+++ Tiara_jewels/Tiara_jewels/settings.py
@@ -128,9 +128,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'aswathivv4@gmail.com'
-# EMAIL_HOST_PASSWORD = 'kyef wsls bpio dacy'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tiarajewels7@gmail.com'
+EMAIL_HOST_PASSWORD = 'eeqq rwhe ajbd gxle'
--- Tiara_jewels/app/views.py
+++ Tiara_jewels/app/views.py
@@ -5,9 +5,9 @@ import os
 from django.contrib.auth.models import User
 from django.contrib import messages
 
-# from django.core.mail import send_mail
-# from django.conf import settings
-# from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponse
 # from django.utils import timezone
 # Create your views here.
 
@@ -45,7 +45,7 @@ def register(req):
         name=req.POST['name']
         email=req.POST['email']
         password=req.POST['password']
-        # send_mail('Eshop registration', 'E_shop account created', settings.EMAIL_HOST_USER, [email])
+        send_mail('Eshop registration', 'E_shop account created', settings.EMAIL_HOST_USER, [email])
         try:
             data=User.objects.create_user(first_name=name,username=email,email=email,password=password)
             data.save()
